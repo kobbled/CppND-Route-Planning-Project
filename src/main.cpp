d41e577eb2de51db4842eba0010643cddcaf9070
kobbled <mattpdewar@gmail.com>
2020-02-14 21:15:54 -0700

    update user input with input flags

--- src/main.cpp
+++ src/main.cpp
@@ -56,10 +56,38 @@ int main(int argc, const char **argv)
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
     float start_x; float start_y; float end_x; float end_y;
-    std::cout << "Enter start coordinates x(0-100) y(0-100):";
-    std::cin >> start_x >> start_y; 
-    std::cout << "Enter end coordinates x(0-100) y(0-100):";
-    std::cin >> end_x >> end_y; 
+    cout << "Enter a start_x from 0 to 100: ";
+    cin >> start_x;
+    while (!(cin) || start_x < 0 || start_x > 100) {
+        cout << "Invalid entry. Enter a start_x from 0 to 100: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        std::cin >> start_x;
+    }
+    cout << "Enter a start_y from 0 to 100: ";
+    cin >> start_y;
+    while (!(cin) || start_y < 0 || start_y > 100) {
+        cout << "Invalid entry. Enter a start_y from 0 to 100: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        std::cin >> start_y;
+    }
+    cout << "Enter a end_x from 0 to 100: ";
+    cin >> end_x;
+    while (!(cin) || end_x < 0 || end_x > 100) {
+        cout << "Invalid entry. Enter a end_x from 0 to 100: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        std::cin >> end_x;
+    }
+    cout << "Enter a end_y from 0 to 100: ";
+    cin >> end_y;
+    while (!(cin) || end_y < 0 || end_y > 100) {
+        cout << "Invalid entry. Enter a end_y from 0 to 100: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        std::cin >> end_y;
+    } 
 
     // Build Model.
     RouteModel model{osm_data};
